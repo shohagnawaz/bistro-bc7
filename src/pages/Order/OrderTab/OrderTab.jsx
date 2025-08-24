4eec65b29142afd1882125f4cca017d7557cfbe7
shohagnawaz <snawazdigital@gmail.com>
2025-08-24 14:35:59 +0600

    login

--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -1,14 +1,37 @@
 import FoodCart from "../../../components/FoodCart/FoodCart";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const OrderTab = ({items}) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    };
+
     return (
-        <div className="grid md:grid-cols-3 gap-10">
-            {
-                items.map(item => <FoodCart
-                    key={item._id}
-                    item={item}
-                ></FoodCart>)
-            }
+        <div>            
+            <Swiper
+                pagination={pagination}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className="grid md:grid-cols-3 gap-10">
+                        {
+                            items.map(item => <FoodCart
+                            key={item._id}
+                            item={item}
+                            ></FoodCart>)
+                        }
+                    </div>
+                </SwiperSlide>
+            </Swiper>
         </div>        
     );
 };
