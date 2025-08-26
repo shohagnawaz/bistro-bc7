ed2702b8160b02a6c5156c0c723a80c816ecf252
shohagnawaz <snawazdigital@gmail.com>
2025-08-26 18:56:54 +0600

    privateRoute

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,73 +1,124 @@
-import { useContext, useEffect, useRef, useState } from 'react';
-import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha 
-} from 'react-simple-captcha';
-import { AuthContext } from '../../providers/AuthProvider';
-import { Link } from 'react-router-dom';
+import { useContext, useEffect, useState } from "react";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from "react-simple-captcha";
+import { AuthContext } from "../../providers/AuthProvider";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
+import Swal from "sweetalert2";
 
 const Login = () => {
+  const [disabled, setDisabled] = useState(true);
+  const { signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
-    const captchaRef = useRef(null);
-    const [disabled, setDisabled] = useState(true);
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
 
-    const { signIn } = useContext(AuthContext)
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    signIn(email, password).then((result) => {
+      const user = result.user;
+      console.log(user);
+      Swal.fire({
+        title: "User Login Successfully",
+        showClass: {
+          popup: `
+      animate__animated
+      animate__fadeInUp
+      animate__faster
+    `,
+        },
+        hideClass: {
+          popup: `
+      animate__animated
+      animate__fadeOutDown
+      animate__faster
+    `,
+        },
+      });
+    });
+    navigate(from, { replace: true });
+  };
 
-    useEffect(() => {
-      loadCaptchaEnginge(6);
-    },[]);
-
-    const handleLogin = event => {
-        event.preventDefault();
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
-        signIn(email, password)
-          .then(result => {
-            const user = result.user;
-            console.log(user)
-          })
+  const handleValidateCaptcha = (e) => {
+    const user_captcha_value = e.target.value;
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
     }
+  };
 
-    const handleValidateCaptcha = () => {
-        const user_captcha_value = captchaRef.current.value;
-        if(validateCaptcha(user_captcha_value)) {
-          setDisabled(false);
-        }
-        else{
-          setDisabled(true);
-        }
-    }
-    
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form onSubmit={handleLogin} className="card-body">
-            <fieldset className="fieldset">
-              <label className="label">Email</label>
-              <input type="email" name="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" name="password" className="input" placeholder="Password" />
-              <label className="label">
-                <LoadCanvasTemplate />
-              </label>
-              <input type="text" ref={captchaRef} name="captcha" className="input" placeholder="type the test above" />
-              <button onClick={handleValidateCaptcha} className="btn btn-outline btn-xm">Validate</button>
-              <input disabled={disabled} className="btn btn-primary mt-4" type="submit" value="Login" />
-            </fieldset>
-          </form>
-          <p><small>New Here? <Link to="/signup">Create an account</Link></small></p>
+    <>
+      <Helmet>
+        <title>Bistro Boss || Login</title>
+      </Helmet>
+
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleLogin} className="card-body">
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="input"
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <label className="label">
+                  <LoadCanvasTemplate />
+                </label>
+                <input
+                  onBlur={handleValidateCaptcha}
+                  type="text"
+                  name="captcha"
+                  className="input"
+                  placeholder="type the test above"
+                />
+                <input
+                  disabled={disabled}
+                  className="btn btn-primary mt-4"
+                  type="submit"
+                  value="Login"
+                />
+              </fieldset>
+            </form>
+            <p className="p-4 ml-4">
+              <small>
+                New Here? <Link to="/signup">Create an account</Link>
+              </small>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,31 +1,106 @@
+import { useContext } from "react";
+import { Helmet } from "react-helmet-async";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+
 const SignUp = () => {
+  const { register, handleSubmit, formState: { errors }, } = useForm();
+  const { createUser } = useContext(AuthContext);
+  const onSubmit = (data) => {
+    console.log(data);
+    createUser(data.email, data.password)
+      .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser)
+      })
+  };
+
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Sign up now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
-            <fieldset className="fieldset">
-              <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
+    <>
+      <Helmet>
+        <title>Bistro Boss || Sign Up</title>
+      </Helmet>
+
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Sign up now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <fieldset className="fieldset">
+                <label className="label">Name</label>
+                <input
+                  type="text"
+                  {...register("name", { required: true })}
+                  className="input"
+                  placeholder="Name"
+                />
+                {errors.name && (
+                  <span className="text-red-600">Name is required</span>
+                )}
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  {...register("email", { required: true })}
+                  className="input"
+                  placeholder="Email"
+                />
+                {errors.email && (
+                  <span className="text-red-600">Email is required</span>
+                )}
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  {...register("password", {
+                    required: true,
+                    minLength: 6,
+                    maxLength: 20,
+                    pattern: /(?=.*[A-Z].)(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/,
+                  })}
+                  className="input"
+                  placeholder="Password"
+                />
+                {errors.password?.type == "required" && (
+                  <p className="text-red-600">Password is required</p>
+                )}
+                {errors.password?.type == "minLength" && (
+                  <p className="text-red-600">Password must be 6 characters</p>
+                )}
+                {errors.password?.type == "maxLength" && (
+                  <p className="text-red-600">
+                    Password must be less then 20 characters
+                  </p>
+                )}
+                {errors.password?.type == "pattern" && (
+                  <p className="text-red-600">
+                    Password must be one uppercase, one lower case, one special
+                    character
+                  </p>
+                )}
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <input
+                  className="btn btn-neutral mt-4"
+                  type="submit"
+                  value="Sign Up"
+                />
+              </fieldset>
+            </form>
+            <p className="p-4 ml-4">
+              <small>Already have an account. <Link to="/login">Please Login</Link></small>
+            </p>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
